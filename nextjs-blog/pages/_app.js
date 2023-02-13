654556141806994c77ccef0e8dc3c4b99bd0dc85
Max <74653870+hesteren@users.noreply.github.com>
2023-02-13 14:14:07 +0100

    simple blog app

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+// The default export of _app.js is a top-level React component that wraps all the pages in your application.
+// You can use this component to keep state when navigating between pages, or to add global styles.
